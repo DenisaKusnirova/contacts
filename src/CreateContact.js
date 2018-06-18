7ab591728a77b9da0da2eefa9e4e65de11590abe
Deniska Kusnirova <deniskakusnirova@gmail.com>
2018-06-18 13:11:07 +0200

    Create createContact component

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import ImageInput from './ImageInput'
 
 class CreateContact extends Component {
     render() {
         return (
-            <div>CreateContact</div>
+            <div>
+                <Link 
+                    className="close-create-contact" 
+                    to="/"
+                />
+                <form className="create-contact-form">
+                    <ImageInput 
+                        className="create-contact-avatar-input"
+                        name="avatarURL"
+                        maxHeight={64}
+                    />
+                    <div className="create-contact-details">
+                        <input type="text" placeholder="Name" name="name" />
+                        <input type="text" placeholder="Handle" name="handle" />
+                        <button>Add contact</button>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
